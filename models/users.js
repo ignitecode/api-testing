7220a708de200bf94bd6f13897329f4ce9ceb321
cbartram <cbartram3@gmail.com>
2019-04-18 13:00:32 -0400

    Initial project boilerplate setup

---
+++ models/users.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const users = sequelize.define('users', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    favoriteFood: DataTypes.STRING,
+    streetAddress: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    zip: DataTypes.INTEGER
+  }, {});
+  users.associate = function(models) {
+    // associations can be defined here
+  };
+  return users;
+};
